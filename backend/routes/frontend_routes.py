f7ac43ed66e33c881446abe244eb8aee4e57eb94
Meghanatp20 <119607547+Meghanatp20@users.noreply.github.com>
2026-04-13 10:01:07 +0530

    error fixed

--- backend/routes/frontend_routes.py
+++ backend/routes/frontend_routes.py
@@ -1,7 +1,7 @@
 import os
 from flask import Blueprint, send_file
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "frontend", "templates")
 STATIC_DIR = os.path.join(BASE_DIR, "frontend", "static")
 
